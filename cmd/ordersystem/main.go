84034e5664f84bf7a3b9bd4ba16aba3beeef05cd
Renan Santos <renansantos@Renans-MacBook-Pro.local>
2024-01-05 10:06:45 -0300

    fix: adjust webserver url

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -48,7 +48,7 @@ func main() {
 	webserver := webserver.NewWebServer(configs.WebServerPort)
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 	webserver.AddHandler("/order", webOrderHandler.Create)
-	webserver.AddHandler("/order", webOrderHandler.List)
+	webserver.AddHandler("/order/", webOrderHandler.List)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
