4d13b3e3a4ad7cf33a632adb05054fda075a73a0
Renan Santos <renansantos@Renans-MacBook-Pro.local>
2024-01-05 09:08:56 -0300

    fix: added values on table orders on migration

--- sql/migrations/000001_init.up.sql
+++ sql/migrations/000001_init.up.sql
@@ -5,3 +5,8 @@ CREATE TABLE orders (
     final_price float NOT NULL, 
     PRIMARY KEY (id)
 )
+
+INSERT INTO orders (id, price, tax, final_price) VALUES ('1', 100, 10, 110);
+INSERT INTO orders (id, price, tax, final_price) VALUES ('2', 100, 10, 110);
+INSERT INTO orders (id, price, tax, final_price) VALUES ('3', 100, 10, 110);
+INSERT INTO orders (id, price, tax, final_price) VALUES ('4', 100, 10, 110);
